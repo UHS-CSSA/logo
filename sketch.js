3963de0e60016f3806539dd6084deeadbcea827b
Timbre Freeman <180427@cvsd356.org>
2018-03-26 09:48:26 -0700

    add a canvas, background and, notes

--- sketch.js
+++ sketch.js
@@ -1,8 +1,13 @@
-
+// Setup run one on start up
 function setup() {
-
+  // this set up the canvas-screen we can drwa in
+  // we put it in a variable to edit later like if we whant the canvas at a certen place
+  var canvas = createCanvas(360, 360);
 }
 
+// Draw run ever frame
 function draw() {
-  
+  // set the bockground cooler
+  background('#de411b');
+
 }
